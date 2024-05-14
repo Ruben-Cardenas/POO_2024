57f3a3c8ebfa325bdf4693f49fa03d8f45886cb2
Ruben-Cardenas <cardenasruben2004@gmail.com>
2024-05-14 11:21:24 -0600

    si

---
+++ parcial1_2A/2_variables_tipos_datos/convertirtiposdatos.py
@@ -0,0 +1,47 @@
+
+
+
+#comvertir de varias lineas 
+#notas: a la hora concatenar cadenas no es posible incluir en algunas ocaciones contenido que no sea del str
+
+# concatenar un str con str 
+
+#texto = "soy una cadena de texto"
+
+# numero =23
+
+# print (texto +  "y soy otra cadena")
+
+
+# concatenar un str con str 
+
+# texto = "soy una cadena de texto"
+
+# numero =23
+
+# numero_str = str(numero)
+
+# print ("El numero : " + numero_str )
+
+# print ("El numero: "+ str (numero))
+
+# concatenar un str con int
+
+n1 = '23'
+n2 = 33 
+
+suma = int(n1)+n2
+
+print ("El numero es: " + str(suma))
+
+# concatenar un float con int con str
+
+n1 = '23'
+n2 = 33.0 # es un float o un real 
+
+suma = int (n1)+n2
+
+print ("El numero es: " + str(int(suma)))
+
+print(F"El numero : {suma}")
+
