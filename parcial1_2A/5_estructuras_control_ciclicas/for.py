95c5756ef9040c6af59f85da6fc30abaaab71978
Ruben-Cardenas <cardenasruben2004@gmail.com>
2024-05-17 11:35:54 -0600

    si

--- parcial1_2A/5_estructuras_control_ciclicas/for.py
+++ parcial1_2A/5_estructuras_control_ciclicas/for.py
@@ -1,24 +1,23 @@
 # Ciclo For estructura iterativa que se ejecuta x veses 
 
 # Sintaxis
-# For variable in elemento _interable   (lista,rango,etc)
+# For variable in elemento _interable   (lista,rango,etc):
 #bloque de instrucciones
 
 # Ejemplo 1 : Crear un programa que programa que imprima en pantalla 5 veses el @
 
-"""
-contador = 1
+
+contador=1
 
 for contador in range (1,5):
     print("@")
     
-"""
-"""
+
 #Ejemplo 2: Crear un programa que imprima los numeros del 1 aal 5 
 # y los sume y al final imprima la suma 
 
 
-contador = 1 
+contador = 1
 suma = 0
 for contador in range (1,6):
     print(contador)
@@ -27,7 +26,7 @@ for contador in range (1,6):
 print (f"la suma es: {suma}")
 
 
-"""
+
 
 #Ejemplo 3: Crear un programa que imprima la tabla de multiplicar que el usuario desee 
 
