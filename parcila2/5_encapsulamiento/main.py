08c5a7b90746672426b25a094a17a78a230c2700
Ruben-Cardenas <cardenasruben2004@gmail.com>
2024-06-26 09:37:27 -0600

    ER Parcial2 POO of VSCode

---
+++ parcila2/5_encapsulamiento/main.py
@@ -0,0 +1,32 @@
+from coches import *
+
+print("Objeto 1")
+coche1=Coches("Blanco","vw","2022",220,150,5)
+
+# # coche1.getInfo()
+
+
+# # print("Objeto 2")
+# # coche2=Coches("Azul","Nissan","2020",180,150,6)
+
+# # coche2.getInfo()
+
+
+#Imprimir el encapsulamiento
+
+
+print (coche1.atributopublico)
+#print (coche1.__atributo__privado)-No es posible esta mal ejecutado
+print (coche1.MetodoPublico())
+#coche__MetodoPrivado()-No es posible esta mal ejecutado
+coche1.getmetodoPublico()
+
+
+
+
+
+
+
+
+
+
