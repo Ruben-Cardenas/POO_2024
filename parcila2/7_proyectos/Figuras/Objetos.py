f3e5c4e4f986934b8a0c7248e3dce5ad99e35ea2
Ruben-Cardenas <cardenasruben2004@gmail.com>
2024-06-27 09:46:29 -0600

    ER Parcial 2 Figuras Geometricas

---
+++ parcila2/7_proyectos/Figuras/Objetos.py
@@ -0,0 +1,11 @@
+from Clases import *
+
+# Ejemplo de uso
+rectangulo = Rectangulo(largo=15, ancho=35)
+print("Área del rectángulo:", rectangulo.calcular_area())
+
+circulo = Circulo(radio=2)
+print("Área del círculo:", circulo.calcular_area())
+
+triangulo = Triangulo(base=18, altura=23)
+print("Área del triángulo:", triangulo.calcular_area())
